f23356bdce10de6e2fe83feb809ffc6fb4e9d7ab
Madeleinesj <m.marchant-daisley@hotmail.com>
2022-12-10 13:19:49 -0800

    Initial commit

---
+++ Javascript/JavaScript_1.js
@@ -0,0 +1,36 @@
+//Switch//
+function Color_Function() {
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+            Color_Output = "Red" + Color_String;
+        break;
+        case "Yellow":
+            Color_Output = "Yellow" + Color_String;
+        break;
+        case "Green":
+            Color_Output = "Green" + Color_String;
+        break;
+        case "Blue":
+            Color_Output = "Blue" + Color_String;
+        break;
+        case "Pink":
+            Color_Output = "Pink" + Color_String;
+        break;
+        case "Purple":
+            Color_Output = "Purple" + Color_String;
+        break;
+        default:
+        Color_Output = "Please enter a color exactly as written on the above list.";
+    }
+    document.getElementById("Output").innerHTML = Color_Output;
+}
+//Get element by class name//
+function Hello() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+//canvas element//
